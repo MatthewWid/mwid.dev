90c7bd9c0881541c11b99b9b3cc270a2a2f6f6b5
Matthew W <matthew.widdi@gmail.com>
2024-03-14 17:30:35 +1100

    feat: redirect /cv to /resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,4 +1,9 @@
+import {Metadata} from "next";
 import {ResumeTitle} from "@/features/resume";
+
+export const metadata: Metadata = {
+	title: "Resume",
+};
 
 export default function ResumePage() {
 	return (
