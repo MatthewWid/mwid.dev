7ecb22b98f4b102928b41f8eb0b031ef59d46860
Matthew W <matthew.widdi@gmail.com>
2024-03-15 14:13:12 +1100

    feat: resume rocket animation scrolls viewport to next section

--- src/features/resume/components/ResumeTitle/resume-title.component.tsx
+++ src/features/resume/components/ResumeTitle/resume-title.component.tsx
@@ -9,25 +9,43 @@ import {Hero} from "@/components/Hero";
 import {ScrollIndicator} from "@/components/ScrollIndicator";
 import styles from "./resume-title.module.scss";
 
+const NEXT_ELEMENT = "#work";
+
 export const ResumeTitle = () => {
 	const [isLaunched, setIsLaunched] = useState(false);
 
+	const rocketOnClick = () => {
+		setIsLaunched(true);
+
+		const targetElement = document.querySelector(NEXT_ELEMENT);
+
+		if (targetElement) {
+			setTimeout(() => {
+				targetElement.scrollIntoView({
+					behavior: "smooth",
+					block: "start",
+				});
+			}, 1000);
+		}
+
+		history.replaceState(null, "", NEXT_ELEMENT);
+	};
+
 	return (
 		<Hero
-			scrollIndicator={
-				<ScrollIndicator to="/resume#work"></ScrollIndicator>
-			}
+			scrollIndicator={<ScrollIndicator to={`/resume${NEXT_ELEMENT}`} />}
 		>
 			<Hero.Title>Resum&eacute;</Hero.Title>
 			<Hero.Subtext>
 				From drone control panels and big-data visualisation tools to
 				supply-chain dashboards and file management systems,
 				<br />I have professional experience working with a wide range
-				of projects across a multitude of diverse teams and organisations.
+				of projects across a multitude of diverse teams and
+				organisations.
 			</Hero.Subtext>
 			<div
 				className={clsx(styles.rocket, isLaunched && styles.launched)}
-				onClick={() => setIsLaunched(true)}
+				onClick={rocketOnClick}
 			>
 				<Image
 					src={rocketPlume}
