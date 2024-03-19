6c42ca9a219ebe996cfd5b7e12c6af2842a17d3c
Matthew W <matthew.widdi@gmail.com>
2024-03-19 18:58:38 +1100

    fix: attribution page semantic html error <ul> nested inside <p>

--- src/features/attribution/components/Attribution/attribution.component.tsx
+++ src/features/attribution/components/Attribution/attribution.component.tsx
@@ -4,17 +4,16 @@ import styles from "./attribution.module.scss";
 export const Attribution = () => (
 	<Hero>
 		<Hero.Title>Attribution</Hero.Title>
-		<Hero.Subtext>
-			<ul className={styles.list}>
-				<li>
-					<a
-						href="https://www.flaticon.com/free-icons/kick-off"
-						title="kick off icons"
-					>
-						Kick off icons created by shin_icons - Flaticon
-					</a>
-				</li>
-			</ul>
-		</Hero.Subtext>
+		<ul className={styles.list}>
+			<li>
+				<a
+					href="https://www.flaticon.com/free-icons/kick-off"
+					title="kick off icons"
+					target="_blank"
+				>
+					Kick off icons created by shin_icons - Flaticon
+				</a>
+			</li>
+		</ul>
 	</Hero>
 );
